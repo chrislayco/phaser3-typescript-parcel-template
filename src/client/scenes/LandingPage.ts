b36d9ff22862982a31a437e673741cf199bc6302
Chris Layco <toffjeoff20@gmail.com>
2021-12-18 12:21:02 -1000

    more attempts at html

--- src/client/scenes/LandingPage.ts
+++ src/client/scenes/LandingPage.ts
@@ -3,10 +3,12 @@ import Phaser, { Loader } from 'phaser'
 export default class LandingPage extends Phaser.Scene
 {
     element = Phaser.GameObjects.DOMElement
+    loader: Phaser.Loader.LoaderPlugin
 
     constructor()
     {
         super('landing-page')
+        //this.loader = new Phaser.Loader.LoaderPlugin(this)
 
     }
 
@@ -14,21 +16,32 @@ export default class LandingPage extends Phaser.Scene
     preload()
     {
         console.log('preloading?')
-        this.load.html('nameform', '/client/scenes/components/nameform.html')
+        //this.load.setPath()
+        //this.load.html('nameform', './components/nameform')
+        this.load.setPath("/client/components")
+        this.load.image('kirby', "kirby.png")
+
+        // https://www.html5gamedevs.com/topic/6606-image-not-displaying-in-phaser/
+        
     }
 
     create()
     {
         console.log('landing page')
-
-
+        
+        this.add.image(200, 200, 'kirby')
 
         this.add.dom(400, 400, 'div', 'background-color: lime; width: 220px; height: 100px; font: 48px Arial', 'Phaser');
         this.add.dom(100, 300, 'div', 'background-color: red; width: 220px; height: 100px; font: 48px Arial', 'Phaser');
 
-        const element = this.add.dom(0, 0).createFromCache('nameform')
+
+
+        //const element = this.add.dom(0, 0).createFromCache('nameform')
         //element.setPerspective(800)
 
+        //this.load.start()
+
+        console.log(this.load.isReady())
 
         //this.add.text(400, 400, 'test')
 
